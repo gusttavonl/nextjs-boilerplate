d8c46e312f5d681c9b979ab1b4c95f20be4d538a
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-07-18 16:16:32 -0300

    refactor: change app descrioption

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The best Game Stores in the world!" />
+        <meta name="description" content="Next Js boilerplate" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
